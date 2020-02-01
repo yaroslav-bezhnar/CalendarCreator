624354bd0e5b42f79f0238096260c03c57f09c14
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2020-02-01 20:27:07 +0200

    enum helper class

---
+++ CalendarCreator.Core/Helpers/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CalendarCreator.Core.Helpers
+{
+    public static class EnumHelper
+    {
+        public static T GetEnumAttribute<T>(this Enum @enum) where T : Attribute
+        {
+            var type = @enum.GetType();
+            var memInfo = type.GetMember(@enum.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+
+            return attributes.Length > 0 ? (T)attributes[0] : null;
+        }
+    }
+}
