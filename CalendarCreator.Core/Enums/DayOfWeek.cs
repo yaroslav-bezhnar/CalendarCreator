a7318d36613e79772aa4853b80d2717ac0899deb
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2020-02-01 20:25:45 +0200

    enum represents days of week

---
+++ CalendarCreator.Core/Enums/DayOfWeek.cs
@@ -0,0 +1,52 @@
+﻿using CalendarCreator.Core.Attributes;
+
+namespace CalendarCreator.Core
+{
+    /// <summary>
+    ///     Specifies the day of the week.
+    /// </summary>
+    public enum DayOfWeek
+    {
+        /// <summary>
+        ///     Represents a Sunday.
+        /// </summary>
+        [ShortName("Sun")]
+        Sunday = 1,
+
+        /// <summary>
+        ///     Represents a Monday.
+        /// </summary>
+        [ShortName("Mon")]
+        Monday = 2,
+
+        /// <summary>
+        ///     Represents a Tuesday.
+        /// </summary>
+        [ShortName("Tue")]
+        Tuesday = 3,
+
+        /// <summary>
+        ///     Represents a Wednesday.
+        /// </summary>
+        [ShortName("Wed")]
+        Wednesday = 4,
+
+        /// <summary>
+        ///     Represents a Thursday.
+        /// </summary>
+        [ShortName("Thu")]
+        Thursday = 5,
+
+        /// <summary>
+        ///     Represents a Friday.
+        /// </summary>
+        [ShortName("Fri")]
+        Friday = 6,
+
+        /// <summary>
+        ///     Represents a Saturday.
+        /// </summary>
+        [ShortName("Sat")]
+        Saturday = 7
+    }
+}
