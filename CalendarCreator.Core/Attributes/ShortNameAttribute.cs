9a107578a86c418f3d65ebca258b65b1e8717e2b
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2020-02-01 20:26:42 +0200

    short name attribute

---
+++ CalendarCreator.Core/Attributes/ShortNameAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CalendarCreator.Core.Attributes
+{
+    /// <summary>
+    ///     Specifies a short name value.
+    /// </summary>
+    public class ShortNameAttribute : Attribute
+    {
+        #region " Constructors "
+
+        /// <summary>
+        ///     Initialize a new instance of the <see cref="ShortNameAttribute" /> class.
+        /// </summary>
+        /// <param name="shortName">Short name.</param>
+        public ShortNameAttribute(string shortName)
+        {
+            ShortName = shortName;
+        }
+
+        #endregion
+
+        #region " Properties "
+
+        /// <summary>
+        ///     Gets/sets a short name.
+        /// </summary>
+        public string ShortName
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
