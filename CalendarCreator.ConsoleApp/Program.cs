6abfe5b0dd0b6e155364cab802acdb3d79764d70
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2020-02-07 00:07:50 +0200

    console app project

---
+++ CalendarCreator.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalendarCreator.ConsoleApp
+{
+    internal static class Program
+    {
+        private static void Main( string[] args )
+        {
+
+        }
+    }
+}
