fb0be1f4b19876a9cc61fdccfb2227432b2903cd
Yaroslav Bezhnar <yaroslav.bezhnar@edvantis.com>
2020-02-13 14:40:26 +0200

    test fixteru to share one instance between test methods

---
+++ CalendarCreator.Tests/TestFixtures/CalendarTestsFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CalendarCreator.Core;
+using DayOfWeek = CalendarCreator.Core.DayOfWeek;
+
+namespace CalendarCreator.Tests
+{
+    public class CalendarTestsFixture : IDisposable
+    {
+        private readonly int _year = 2008;
+        private readonly DayOfWeek _firstYearDay = DayOfWeek.Tuesday;
+
+        public CalendarTestsFixture()
+        {
+            Calendar = new Calendar(_year, _firstYearDay);
+        }
+
+        public Calendar Calendar
+        {
+            get;
+            private set;
+        }
+
+        public void Dispose()
+        {
+            Calendar = null;
+        }
+    }
+}
